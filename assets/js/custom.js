202ee569bbde82e8698135280ff14bf8ef27ff99
unknown <you@example.com>
2022-05-10 15:35:53 +0300

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('current');
+        })
+        $(this).addClass('current');
+      
+        var target = this.hash,
+            header = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#mainNav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#mainNav ul li a').removeClass("current");
+            currLink.addClass("current");
+        }
+        else{
+            currLink.removeClass("current");
+        }
+    });
+}
